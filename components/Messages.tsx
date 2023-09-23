16af647a8c07a32bb88819724daf875643d37900
ccsunnyfd <chzhyfd@126.com>
2023-09-23 17:22:33 +0800

    [fix]Image sizes prop

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -89,6 +89,7 @@ const Messages: FC<MessagesProps> = ({
               >
                 <Image
                   fill
+                  sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   src={
                     isCurrentUser ? (sessionImg as string) : chatPartner.image
                   }
