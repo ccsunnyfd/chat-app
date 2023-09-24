a9e9699b59969dbe814adee4594c100ed837e7aa
ccsunnyfd <chzhyfd@126.com>
2023-09-24 19:04:39 +0800

    [feat]chat realtime feature

--- app/api/message/send/route.ts
+++ app/api/message/send/route.ts
@@ -49,6 +49,15 @@ export async function POST(req: Request) {
 
         const message = messageValidator.parse(messageData)
 
+        // notify all connected chat room clients
+        await pusherServer.trigger(toPusherKey(`chat:${chatId}`), 'incoming-message', message)
+
+        await pusherServer.trigger(toPusherKey(`user:${friendId}:chats`), 'new_message', {
+            ...message,
+            senderImg: sender.image,
+            senderName: sender.name
+        })
+
         // all valid, send the message
         await db.zadd(`chat:${chatId}:messages`, {
             score: timestamp,
